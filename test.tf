b18493cccc7375c6bc5eb652d924f0966ae35586
chetanbhatia99 <63634065+chetanbhatia99@users.noreply.github.com>
2020-04-27 21:30:41 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -10,6 +10,6 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "rg" {
-        name = "testResourceGroup"
+        name = "TerraformResourceGroup"
         location = "westus"
 }
